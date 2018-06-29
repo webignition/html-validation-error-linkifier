3c17483f748eacaa105a9594dcab8aaddab1b71a
Jon Cram <jon@webignition.net>
2018-06-29 17:21:09 +0100

    PSR4 autoloading

--- src/HtmlValidationErrorLinkifier.php
+++ src/HtmlValidationErrorLinkifier.php
@@ -2,91 +2,89 @@
 
 namespace webignition\HtmlValidationErrorLinkifier;
 
-use webignition\HtmlValidationErrorNormaliser\HtmlValidationErrorNormaliser;
-
 class HtmlValidationErrorLinkifier {
-    
+
     const START_PLACEHOLDER_CHARACTER = 'x';
-    
+
     private $normalForm = '';
     private $linkifiedForm = '';
-    
+
     public function linkify($normalForm, $parameters = null) {
         $this->normalForm = $normalForm;
         $this->linkified = $normalForm;
-        
+
         $this->replacePlaceholders($parameters);
         $this->replaceAmpWithAmpserandPlaceholder();
         $this->removePunctuation();
         $this->replaceSpaces();
-        $this->replaceSlashes();        
+        $this->replaceSlashes();
         $this->replaceAmpersandPlaceholder();
-        $this->replaceDoubleHyphens();        
-        
-        return $this->linkifiedForm;        
+        $this->replaceDoubleHyphens();
+
+        return $this->linkifiedForm;
     }
-    
+
     private function replaceDoubleHyphens() {
         while (substr_count($this->linkifiedForm, '--') > 0) {
             $this->linkifiedForm = str_replace('--', '-', $this->linkifiedForm);
-        }        
+        }
     }
-    
+
     private function replaceAmpersandPlaceholder() {
         $this->linkifiedForm = str_replace('&', 'ampersand', $this->linkifiedForm);
         $this->linkifiedForm = str_replace('ampersand_placeholder', 'amp', $this->linkifiedForm);
-    }    
-    
+    }
+
     private function replaceAmpWithAmpserandPlaceholder() {
         $this->linkifiedForm = str_replace('&amp', 'ampersand_placeholder', $this->linkifiedForm);
     }
-    
-    
+
+
     private function  replacePlaceholders($parameters = null) {
         $placeholders = $this->getPlaceholders();
         $placeholderValues = $this->getPlaceholderValues($parameters);
-       
-        $this->linkifiedForm = strtolower(str_replace($placeholders, $placeholderValues, $this->normalForm));        
+
+        $this->linkifiedForm = strtolower(str_replace($placeholders, $placeholderValues, $this->normalForm));
     }
 
     private function replaceSlashes() {
         $this->linkifiedForm = str_replace('/', '-slash-', $this->linkifiedForm);
-    }    
-    
+    }
+
     private function replaceSpaces() {
         $this->linkifiedForm = str_replace(' ', '-', $this->linkifiedForm);
     }
-    
+
     private function removePunctuation() {
         $this->linkifiedForm = str_replace(array(
             ',', '.', ':', ';', '"', '(', ')','/>'
         ), '', $this->linkifiedForm);
     }
-    
-    
+
+
     /**
-     * 
+     *
      * @return array
      */
     private function getPlaceholders() {
-        $placeholderMatches = array();        
-        
+        $placeholderMatches = array();
+
         if (preg_match_all('/%[0-9]/', $this->normalForm, $placeholderMatches) === false) {
             return array();
         }
-        
+
         return $placeholderMatches[0];
     }
-    
-    
+
+
     /**
-     * 
+     *
      * @return int
      */
     private function getPlaceholderCount() {
         return count($this->getPlaceholders());
     }
-    
+
     private function getPlaceholderValues($parameters = null) {
         $start = ord(self::START_PLACEHOLDER_CHARACTER);
         $placeholderCount = $this->getPlaceholderCount();
@@ -100,7 +98,7 @@ class HtmlValidationErrorLinkifier {
         for ($index = $start; $index < $start + $placeholderCount; $index++) {
             $placeholderValues[] = chr($index);
         }
-        
+
         if (is_array($parameters)) {
             foreach ($parameters as $key => $value) {
                 if (isset($placeholderValues[$key])) {
@@ -109,7 +107,7 @@ class HtmlValidationErrorLinkifier {
             }
         }
 
-        return $placeholderValues;     
+        return $placeholderValues;
     }
-    
+
 }
--- tests/IntegrityTest.php
+++ tests/IntegrityTest.php
@@ -4,20 +4,20 @@ namespace webignition\HtmlValidationErrorLinkifier\Tests\webignition\HtmlValidat
 
 use webignition\HtmlValidationErrorLinkifier\HtmlValidationErrorLinkifier;
 
-class IntegrityTest extends \PHPUnit_Framework_TestCase {    
-    
+class IntegrityTest extends \PHPUnit_Framework_TestCase {
+
     public function testIntegrity() {
-        $errors = file(__DIR__ . '/../../fixtures/normalised-error-strings.txt');        
+        $errors = file(__DIR__ . '/fixtures/normalised-error-strings.txt');
         $linkifier = new HtmlValidationErrorLinkifier();
-        
+
         foreach ($errors as $error) {
             $error = trim($error);
             $linkifier->linkify($error);
-            
+
 //            var_dump($error);
 //            var_dump($linkifier->linkify($error));
-//            print("\n");            
+//            print("\n");
         }
-    }      
-    
+    }
+
 }
